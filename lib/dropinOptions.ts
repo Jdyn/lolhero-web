b2c82036e4a0d258f0bd76a0ca9fd955c62ddca2
Jaden <moorejaden5389@gmail.com>
2019-12-20 12:12:46 -0800

    any type on  options

--- lib/dropinOptions.ts
+++ lib/dropinOptions.ts
@@ -1,6 +1,6 @@
 import { Options } from 'braintree-web-drop-in';
 
-const options: Options = {
+const options: any = {
   authorization: 'sandbox_7brmzhhx_cfcsbff65qmxzrgf', // process.env.NODE_ENV === 'production' ? '' : 'sandbox_7brmzhhx_cfcsbff65qmxzrgf',
   container: '#dropin-container',
   card: {
