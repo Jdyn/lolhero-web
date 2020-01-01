1331fe8b1c8467f018608a50c7a38efeb636e6a2
Jaden <moorejaden5389@gmail.com>
2019-12-31 19:11:18 -0800

    new rank colors

--- lib/ranks.ts
+++ lib/ranks.ts
@@ -10,130 +10,130 @@ const ranks = [
     {
       title: 'Iron IV',
       rank: 1,
-      color: '#3f5461',
-      accent: '#26323A'
+      color: '#616161',
+      accent: '#424242'
     },
     {
       title: 'Iron III',
       rank: 2,
-      color: '#3f5461',
-      accent: '#26323A'
+      color: '#616161',
+      accent: '#424242'
     },
     {
       title: 'Iron II',
       rank: 3,
-      color: '#3f5461',
-      accent: '#26323A'
+      color: '#616161',
+      accent: '#424242'
     },
     {
       title: 'Iron I',
       rank: 4,
-      color: '#3f5461',
-      accent: '#26323A'
+      color: '#616161',
+      accent: '#424242'
     }
   ],
   [
     {
       title: 'Bronze IV',
       rank: 5,
-      color: '#863d25',
-      accent: '#502516'
+      color: '#6D4C41',
+      accent: '#4E342E'
     },
     {
       title: 'Bronze III',
       rank: 6,
-      color: '#863d25',
-      accent: '#502516'
+      color: '#6D4C41',
+      accent: '#4E342E'
     },
     {
       title: 'Bronze II',
       rank: 7,
-      color: '#863d25',
-      accent: '#502516'
+      color: '#6D4C41',
+      accent: '#4E342E'
     },
     {
       title: 'Bronze I',
       rank: 8,
-      color: '#863d25',
-      accent: '#502516'
+      color: '#6D4C41',
+      accent: '#4E342E'
     }
   ],
   [
     {
       title: 'Silver IV',
       rank: 9,
-      color: '#616161',
-      accent: '#424242'
+      color: '#546E7A',
+      accent: '#37474F'
     },
     {
       title: 'Silver III',
       rank: 10,
-      color: '#616161',
-      accent: '#424242'
+      color: '#546E7A',
+      accent: '#37474F'
     },
     {
       title: 'Silver II',
       rank: 11,
-      color: '#616161',
-      accent: '#424242'
+      color: '#546E7A',
+      accent: '#37474F'
     },
     {
       title: 'Silver I',
       rank: 12,
-      color: '#616161',
-      accent: '#424242'
+      color: '#546E7A',
+      accent: '#37474F'
     }
   ],
   [
     {
       title: 'Gold IV',
       rank: 13,
-      color: '#d89926',
-      accent: '#825C17'
+      color: '#FFB300',
+      accent: '#FF8F00'
     },
     {
       title: 'Gold III',
       rank: 14,
-      color: '#d89926',
-      accent: '#825C17'
+      color: '#FFB300',
+      accent: '#FF8F00'
     },
     {
       title: 'Gold II',
       rank: 15,
-      color: '#d89926',
-      accent: '#825C17'
+      color: '#FFB300',
+      accent: '#FF8F00'
     },
     {
       title: 'Gold I',
       rank: 16,
-      color: '#d89926',
-      accent: '#825C17'
+      color: '#FFB300',
+      accent: '#FF8F00'
     }
   ],
   [
     {
       title: 'Platinum IV',
       rank: 17,
-      color: '#50b3a4',
-      accent: '#306B62'
+      color: '#009688',
+      accent: '#00695C'
     },
     {
       title: 'Platinum III',
       rank: 18,
-      color: '#50b3a4',
-      accent: '#306B62'
+      color: '#009688',
+      accent: '#00695C'
     },
     {
       title: 'Platinum II',
       rank: 19,
-      color: '#50b3a4',
-      accent: '#306B62'
+      color: '#009688',
+      accent: '#00695C'
     },
     {
       title: 'Platinum I',
       rank: 20,
-      color: '#50b3a4',
-      accent: '#306B62'
+      color: '#009688',
+      accent: '#00695C'
     }
   ],
   [
