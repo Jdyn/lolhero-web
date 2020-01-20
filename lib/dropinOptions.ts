8560074732bfe19da0c5b2a88483bb3949014888
Jaden <moorejaden5389@gmail.com>
2020-01-19 17:20:45 -0800

    wow

--- lib/dropinOptions.ts
+++ lib/dropinOptions.ts
@@ -3,7 +3,7 @@
 const options: any = {
   authorization:
     process.env.NODE_ENV === 'production'
-      ? 'production_kt5hfrt9_rjbznr7xz82wjj7x	'
+      ? 'production_kt5hfrt9_rjbznr7xz82wjj7x'
       : 'sandbox_7brmzhhx_cfcsbff65qmxzrgf',
   container: '#dropin-container',
   card: {
