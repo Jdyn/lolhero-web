058ce82a81275b7446bae9fba3937e8f2088f063
Jaden <moorejaden5389@gmail.com>
2020-02-24 22:48:59 -0800

    disable pages

--- components/Home/HomeInfo/HomeInfo.tsx
+++ components/Home/HomeInfo/HomeInfo.tsx
@@ -4,16 +4,18 @@ import styles from './styles.module.css';
 const HomeInfo = (): JSX.Element => {
   return (
     <section className={styles.root}>
-      <div className={styles.container} />
-      <div className={styles.content}>
-        <div className={styles.hero}>
-          <h1>Stuck in elo hell?</h1>
-          <p>
-            If you’re stuck in elo hell and your teammates are continously holding you back, we are
-            here to help. Getting unlucky teammates is frustrating and can make it feel impossible
-            to climb. we can give you a slight push in the right direction!
-          </p>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.hero}>
+            <h1>Stuck in elo hell?</h1>
+            <p>
+              If you’re stuck in elo hell and your teammates are continously holding you back, we
+              are here to help. Getting unlucky teammates is frustrating and can make it feel
+              impossible to climb. we can give you a slight push in the right direction!
+            </p>
+          </div>
         </div>
+        <div className={styles.image} />
       </div>
     </section>
   );
